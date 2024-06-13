ab7509b6b9615a939e1f44790c01638df4a8db9d
solivera2 <53319338+solivera2@users.noreply.github.com>
2024-06-13 11:32:36 -0300

    Update main.ts

---
+++ main.ts
@@ -0,0 +1,8 @@
+namespace dibujoenpantalla {
+    export function plotAt(index: number): void {
+        index |= 0
+        const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+        led.plot(x, y)
+    }
+}
